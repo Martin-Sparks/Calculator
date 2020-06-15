ad6a27359822ca4a161ba0b2f836b5faacc8d0e4
mark <markpackham1@gmail.com>
2020-06-15 15:29:29 +0100

    Initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calc;
+
+    @Before
+    public void before(){
+        calc = new Calculator();
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals();
+    }
+}
